4b6c6ce56f002844440f25adcc09a16cae7cd0b4
Sina Sotoudeh <s.sotoudeh1@gmail.com>
2026-04-20 10:50:46 +0330

    all old and unsed components removed and page.tsx modified according to this permanent removals.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
       <Manifesto />
       <WorkMinimal />
       <CapabilitiesSection />
-      {/* <OrbitalProcess /> */}
       <ProcessSection />
       <ResumeDashboard />
       <Contact />
